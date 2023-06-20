2ab342469ecb0e1dff2372862dec5db4928f0f48
Muhammad Awais <70286513+awaisshahid965@users.noreply.github.com>
2023-06-21 00:00:04 +0500

    test

--- enumToCaptalize.ts
+++ enumToCaptalize.ts
@@ -2,4 +2,5 @@ export const enumToCaptalize = (val: string) => {
     if (!val) {
         return val;
     }
+    return val.split('_').map(v => v[0].toLocaleLowerCase() + v.slice(1, v.length)).join(' ')
 }
