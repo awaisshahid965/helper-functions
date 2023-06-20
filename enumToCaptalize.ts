cb24625cac007a146c639bbdf218755545a8dcdc
Muhammad Awais <70286513+awaisshahid965@users.noreply.github.com>
2023-06-21 00:03:35 +0500

    completed enum to captalize

--- enumToCaptalize.ts
+++ enumToCaptalize.ts
@@ -2,5 +2,5 @@ export const enumToCaptalize = (val: string) => {
     if (!val) {
         return val;
     }
-    return val.split('_').map(v => v[0].toLocaleLowerCase() + v.slice(1, v.length)).join(' ')
+    return val.split('_').map(v => v[0].toUpperCase() + v.slice(1, v.length).toLowerCase()).join(' ')
 }
