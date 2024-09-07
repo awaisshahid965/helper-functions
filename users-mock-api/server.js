8da250c736305649980cdfb06b43e09cd22251ee
Muhammad Awais <awaisshahid965@gmail.com>
2024-09-08 00:41:55 +0500

    feat: added mock api

---
+++ users-mock-api/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const usersData = require('./data.json')
+
+const PORT = 3000
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+
+    res.json({
+        users: {
+            admin: usersData.slice(0, 50),
+            normal: usersData.slice(50, usersData.length),
+        },
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`server listening at port ${PORT}`)
+})
